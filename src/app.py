3091aaa67dfa42a1732f70503dd3a8fddde731cf
abcd <abcd@apples-MacBook-Pro.local>
2024-06-21 18:07:51 +0530

    updated stocks code

--- src/app.py
+++ src/app.py
@@ -1,100 +1,121 @@
-'''
-# A simple app demonstrating how to dynamically render tab content containing
-# dcc.Graph components to ensure graphs get sized correctly. We also show how
-# dcc.Store can be used to cache the results of an expensive graph generation
-# process so that switching tabs is fast.
-# Source: https://dash-bootstrap-components.opensource.faculty.ai/examples/graphs-in-tabs/
-# @ Create Time: 2024-06-21 15:35:46.868063
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-'''
-import time
+import requests
+import pandas as pd
+import io
+import yfinance as yf
+import matplotlib.pyplot as plt
+from decimal import Decimal, getcontext
+from dash import Dash, html, dash_table, dcc,callback, Output, Input
+#from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import pandas as pd
+import plotly.express as px
 
-import dash
-import dash_bootstrap_components as dbc
-import numpy as np
-import plotly.graph_objs as go
-from dash import Input, Output, dcc, html
 
-app = dash.Dash(
-    title="DStocksApp1",
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    )
+nse_url = "https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv"
 
-# Declare server for Heroku deployment. Needed for Procfile.
-server = app.server
+# create Session from 'real' browser
+headers = {
+    'User-Agent': 'Mozilla/5.0'
+}
 
-app.layout = dbc.Container(
-    [
-        dcc.Store(id="store"),
-        html.H1("Dynamically rendered tab content - DStocksApp1"),
-        html.Hr(),
-        dbc.Button(
-            "Regenerate graphs",
-            color="primary",
-            id="button",
-            className="mb-3",
-        ),
-        dbc.Tabs(
-            [
-                dbc.Tab(label="Scatter", tab_id="scatter"),
-                dbc.Tab(label="Histograms", tab_id="histogram"),
-            ],
-            id="tabs",
-            active_tab="scatter",
-        ),
-        html.Div(id="tab-content", className="p-4"),
-    ]
+s = requests.Session()
+s.headers.update(headers)
+
+# do a get call now
+url = 'https://nsearchives.nseindia.com/content/equities/EQUITY_L.csv'
+r = s.get(nse_url)
+s.close()
+
+# saving it to pd df for further preprocessing
+df_nse = pd.read_csv(io.BytesIO(r.content))
+df_nse['SYMBOLNSE']=df_nse['SYMBOL'].astype(str)+'.NS'
+
+
+
+data = yf.download(df_nse['SYMBOLNSE'].iloc[1980], start='2019-06-19', end='2024-06-19')
+data = data.reset_index()
+# Initialize the app
+# Initialize the app - incorporate css
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(external_stylesheets=external_stylesheets)
+
+app = Dash()
+
+# # App layout
+# app.layout = [
+#     html.Div(children='NSE Equity Data     '+     df_nse['SYMBOLNSE'].iloc[1980]),
+#     # dash_table.DataTable(data=data.to_dict('records'), page_size=10),
+#     # dcc.Graph(figure=px.histogram(data, x='Date', y='Close', histfunc='avg'))
+
+
+#     html.Hr(),
+#     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+#     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+#     dcc.Graph(figure={}, id='controls-and-graph')
+
+
+
+# ]
+
+
+
+
+
+# app.layout = [
+#     html.Div(children='NSE Equity Data     '+     df_nse['SYMBOLNSE'].iloc[1980]'),
+#     html.Hr(),
+#     dcc.RadioItems(options=['Open', 'High', 'Low','Close','Adj Close','Volume'], value='Close', id='controls-and-radio-item'),
+#     dash_table.DataTable(data=data.to_dict('records'), page_size=6),
+#     dcc.Graph(figure={}, id='controls-and-graph')
+# ]
+
+
+
+app.layout = [
+    html.Div(className='row', children='NSE Equity Data     '+     df_nse['SYMBOL'].iloc[1980],
+             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+    html.Div(className='row', children=[
+        dcc.RadioItems(options=['Open', 'High', 'Low','Close','Adj Close','Volume'],
+                       value='Close',
+                       inline=True,
+                       id='my-radio-buttons-final')
+    ]),
+
+    html.Div(className='row', children=[
+        html.Div(className='six columns', children=[
+            dash_table.DataTable(data=data.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+        ]),
+        html.Div(className='six columns', children=[
+            dcc.Graph(figure={}, id='histo-chart-final')
+        ])
+    ])
+]
+
+# Add controls to build the interaction
+@callback(
+
+    Output(component_id='histo-chart-final', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
 )
+def update_graph(col_chosen):
+    fig = px.histogram(data, x='Date', y=col_chosen, histfunc='avg')
+    return fig
 
 
-@app.callback(
-    Output("tab-content", "children"),
-    [Input("tabs", "active_tab"), Input("store", "data")],
-)
-def render_tab_content(active_tab, data):
-    """
-    This callback takes the 'active_tab' property as input, as well as the
-    stored graphs, and renders the tab content depending on what the value of
-    'active_tab' is.
-    """
-    if active_tab and data is not None:
-        if active_tab == "scatter":
-            return dcc.Graph(figure=data["scatter"])
-        elif active_tab == "histogram":
-            return dbc.Row(
-                [
-                    dbc.Col(dcc.Graph(figure=data["hist_1"]), width=6),
-                    dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
-                ]
-            )
-    return "No tab selected"
+# # Add controls to build the interaction
+# @callback(
+#     Output(component_id='controls-and-graph', component_property='figure'),
+#     Input(component_id='controls-and-radio-item', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(data, x='Date', y=col_chosen, histfunc='avg')
+#     return fig
 
 
-@app.callback(Output("store", "data"), [Input("button", "n_clicks")])
-def generate_graphs(n):
-    """
-    This callback generates three simple graphs from random data.
-    """
-    if not n:
-        # generate empty graphs when app loads
-        return {k: go.Figure(data=[]) for k in ["scatter", "hist_1", "hist_2"]}
 
-    # simulate expensive graph generation process
-    time.sleep(2)
-
-    # generate 100 multivariate normal samples
-    data = np.random.multivariate_normal([0, 0], [[1, 0.5], [0.5, 1]], 100)
-
-    scatter = go.Figure(
-        data=[go.Scatter(x=data[:, 0], y=data[:, 1], mode="markers")]
-    )
-    hist_1 = go.Figure(data=[go.Histogram(x=data[:, 0])])
-    hist_2 = go.Figure(data=[go.Histogram(x=data[:, 1])])
-
-    # save figures in a dictionary for sending to the dcc.Store
-    return {"scatter": scatter, "hist_1": hist_1, "hist_2": hist_2}
-
+# Run the app
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8050)
